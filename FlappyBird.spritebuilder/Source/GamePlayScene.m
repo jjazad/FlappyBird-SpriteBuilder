9449364c1ad59c7e04223d73ea846ab1c55f7162
John <jzofrea@yahoo.com>
2014-06-26 13:47:39 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,28 @@
 - (void)initialize
 {
     // your code here
+
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicisNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+
+
 }
 
 // put new methods here
+
+
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+
+        // This method will be called everytime a player touches the screen.
+
+    [character flap];
+}
+
+
 
 @end
